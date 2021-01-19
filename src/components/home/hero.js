9267cb431ef8797cd335eb7f544ee73e2d0beefc
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2021-01-19 18:59:10 +0700

    initial-commit

---
+++ src/components/home/hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Carousel } from "antd";
+function AppHero() {
+  return (
+    <div className="heroBlock">
+      <Carousel afterChange={onChange}>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+export default AppHero;
