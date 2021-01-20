8fe7da2a07abaf4392b0e2ae4b8fe6e51dfd5695
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2021-01-20 23:21:43 +0700

    add-faq-pricing

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,7 +1,9 @@
 import React from "react";
 import AppAbout from "../home/about";
+import AppFaq from "../home/faq";
 import AppFeature from "../home/feature";
 import AppHero from "../home/hero";
+import AppPricing from "../home/pricing";
 import AppWorks from "../home/works";
 const AppHome = () => {
   return (
@@ -10,6 +12,8 @@ const AppHome = () => {
       <AppAbout />
       <AppFeature />
       <AppWorks />
+      <AppFaq />
+      <AppPricing />
     </div>
   );
 };
